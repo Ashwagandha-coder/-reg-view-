6e3f0bc0f49588aea65ff8c03e6f46cb4dcb26d3
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-05-04 17:50:19 +0300

    base impl CloudDataSource.kt

--- app/src/main/java/com/example/religionapp/data/network/CloudDataSource.kt
+++ app/src/main/java/com/example/religionapp/data/network/CloudDataSource.kt
@@ -6,4 +6,9 @@ interface CloudDataSource {
 
     suspend fun fetchBooks(): List<BooksData>
 
+
+    class Base(private val service: BookService): CloudDataSource {
+        override suspend fun fetchBooks(): List<BooksData> = service.fetchBooks()
+    }
+
 }
