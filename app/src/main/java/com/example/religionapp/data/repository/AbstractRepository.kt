c05d3f75923bdfeb00e14c1100e181bc99f22871
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-05-13 23:23:41 +0300

    [add] repo

---
+++ app/src/main/java/com/example/religionapp/data/repository/AbstractRepository.kt
@@ -0,0 +1,10 @@
+package com.example.religionapp.data.repository
+
+interface AbstractRepository {
+
+    interface BaseRepository : AbstractRepository
+
+    interface SecondRepository : AbstractRepository
+
+
+}
--- app/src/main/java/com/example/religionapp/data/repository/Repository.kt
+++
@@ -1,4 +0,0 @@
-package com.example.religionapp.data.repository
-
-interface Repository {
-}
