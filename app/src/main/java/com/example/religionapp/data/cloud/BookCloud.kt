3245cb8d4c93444f3d5792a008cce3ef216e56e8
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-05-13 23:24:22 +0300

    [add] impl

--- app/src/main/java/com/example/religionapp/data/cloud/BookCloud.kt
+++ app/src/main/java/com/example/religionapp/data/cloud/BookCloud.kt
@@ -1,4 +1,7 @@
 package com.example.religionapp.data.cloud
 
-class BookCloud {
+class BookCloud : BookCloudDataSource {
+    override suspend fun fetchBooks(): List<BookData> {
+        TODO("Not yet implemented")
+    }
 }
