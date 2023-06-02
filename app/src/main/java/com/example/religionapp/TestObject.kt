fe4f9d9a67219af734af735626fbf6e0bcb1c232
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-06-03 00:15:29 +0300

    add base api for TestObject.kt

--- app/src/main/java/com/example/religionapp/TestObject.kt
+++ app/src/main/java/com/example/religionapp/TestObject.kt
@@ -1,4 +1,23 @@
 package com.example.religionapp
 
+import java.util.concurrent.atomic.AtomicLong
+
 object TestObject {
+
+    private val atomicLong = AtomicLong(0L)
+
+    fun update() {}
+
+    fun get() {}
+
+    fun set() {}
+
+    fun increment() {}
+
+    fun decrement() {}
+
+
+    fun compareAndSet() {}
+
+
 }
