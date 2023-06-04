e5ce4198dec0388c86f52f4c9094a883dc2222bc
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-06-04 20:40:58 +0300

    refactor package

--- app/src/main/java/com/example/religionapp/other/TestCompanionObject.kt
+++ app/src/main/java/com/example/religionapp/other/TestCompanionObject.kt
@@ -1,4 +1,4 @@
-package com.example.religionapp
+package com.example.religionapp.other
 
 class TestCompanionObject {
 
--- app/src/main/java/com/example/religionapp/other/TestObject.kt
+++ app/src/main/java/com/example/religionapp/other/TestObject.kt
@@ -1,4 +1,4 @@
-package com.example.religionapp
+package com.example.religionapp.other
 
 import java.util.concurrent.atomic.AtomicLong
 
