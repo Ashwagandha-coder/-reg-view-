508324292e4da880165e255c33b0da59b12a557c
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-06-10 17:51:00 +0300

    input

--- app/src/main/java/com/example/religionapp/other/ImplX509.kt
+++ app/src/main/java/com/example/religionapp/other/ImplX509.kt
@@ -1,12 +1,14 @@
 package com.example.religionapp.other
 
 import java.io.DataInput
+import java.io.InputStream
 import java.security.cert.X509Certificate
 import javax.net.ssl.X509TrustManager
 
 class ImplX509 : X509TrustManager {
 
     private val dataInput: DataInput? = null
+    private val inputStream: InputStream? = null
 
     override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
         TODO("Not yet implemented")
